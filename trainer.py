85818951d371cbcd15c489d30623783eebacb8cc
R0nSam <138660046+R0nSam@users.noreply.github.com>
2024-12-11 14:17:54 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -52,7 +52,7 @@ class Trainer:
             self.opt.frame_ids.append("s")
 
         self.models["encoder"] = networks.ResnetEncoder(
-            self.opt.num_layers, self.opt.weights_init == "pretrained")
+            self.opt.num_layers, self.opt.weights_init == "pretrained", num_input_images=2) # changed here no. of input images
         self.models["encoder"].to(self.device)
         self.parameters_to_train += list(self.models["encoder"].parameters())
 
@@ -245,7 +245,9 @@ class Trainer:
             outputs = self.models["depth"](features[0])
         else:
             # Otherwise, we only feed the image with frame_id 0 through the depth encoder
-            features = self.models["encoder"](inputs["color_aug", 0, 0])
+            all_color_aug = torch.cat([inputs[("color_aug", i, 0)] for i in self.opt.frame_ids[:-1]])
+            features = self.models["encoder"](all_color_aug)
+            #features = self.models["encoder"](inputs["color_aug", 0, 0])
             outputs = self.models["depth"](features)
 
         if self.opt.predictive_mask:
